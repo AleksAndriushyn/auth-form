94d1bea495c8c896b23a17d1bb8064aca7df9a2f
Алексей Андрюшин <aleksroebuck@gmail.com>
2024-08-25 23:57:56 +0300

    change base url to production

--- bulls-media/src/services/utils.js
+++ bulls-media/src/services/utils.js
@@ -1,1 +1,4 @@
-export const baseUrl = 'http://localhost:3000';
+export const baseUrl =
+	process.env.NODE_ENV === 'production'
+		? 'https://auth-form-ten.vercel.app/'
+		: 'http://localhost:3000';
